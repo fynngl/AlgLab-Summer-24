26e8ca63de10262d1180ff1449036efffe5e8046
Fynn Gläser <f.glaeser@tu-braunschweig.de>
2024-04-07 17:23:23 +0200

    cpsat kinda done, not without add functions

--- sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
@@ -8,8 +8,18 @@ def solve(instance: Instance) -> Solution:
     """
     numbers = instance.numbers
     model = cp_model.CpModel()
+    x = model.NewIntVar(min(numbers),max(numbers), "x")
+    y = model.NewIntVar(min(numbers),max(numbers), "y")
+    abs_diff = model.NewIntVar(0,max(numbers)-min(numbers), "abs_diff")
+    #model.add(abs_diff == max(x,y)-min(x,y))
+    model.AddAbsEquality(abs_diff, x - y)
+    model.Maximize(abs_diff)
+    
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+    #print(status)
     return Solution(
-        number_a=numbers[0],
-        number_b=numbers[-1],
-        distance=abs(numbers[0] - numbers[-1]),
+        number_a=solver.value(x),
+        number_b=solver.value(y),
+        distance=abs(solver.value(x)-solver.value(y)),
     )
