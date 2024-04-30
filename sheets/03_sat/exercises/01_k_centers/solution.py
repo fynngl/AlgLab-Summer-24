86c64fc1659dc2e2941cca8169464a84d3c75fdf
Fynn Gläser <f.glaeser@tu-braunschweig.de>
2024-04-30 19:00:36 +0200

    k centers done

--- sheets/03_sat/exercises/01_k_centers/solution.py
+++ sheets/03_sat/exercises/01_k_centers/solution.py
@@ -18,16 +18,32 @@ class KCentersSolver:
         Check the networkx documentation for more information!
         """
         self.graph = graph
-        # TODO: Implement me!
+        self.distances = {}
+        self.vars = {node: i+1 for i, node in enumerate(graph.nodes)}
+        self.reverse = {i: node for node, i in self.vars.items()}
+        self.x = [self.vars[v] for v in self.graph.nodes]
+        for v in self.graph.nodes():
+            for w in self.graph.nodes():
+                    self.distances[(v,w)] = nx.dijkstra_path_length(self.graph, v, w)
+        #print(f"dis: {self.distances}")
+                    
 
-    def solve_heur(self, k: int) -> List[int]:
+    def solve_heur(self, k: int, m: int) -> List[int]:
         """
         Calculate a heuristic solution to the k-centers problem.
         Returns the k selected centers as a list of ints.
         (nodes will be ints in the given graph).
         """
-        # TODO: Implement me!
-        centers = None
+        centers = []
+        sat = SATSolver("MiniCard")
+        sat.add_atmost(self.x, k)
+        for v in self.graph.nodes:
+            in_range = [w for w in self.graph.nodes if self.distances[(v,w)] <= m]
+            #print(f"inrange: {in_range}")
+            sat.add_clause(in_range)
+        #print(f"solve: {sat.solve()}")            
+        if sat.solve():
+            centers = sat.get_model()
         return centers
 
 
@@ -36,9 +52,28 @@ class KCentersSolver:
         For the given parameter k, calculate the optimal solution
         to the k-centers solution and return the selected centers as a list.
         """
-        sat = SATSolver("MiniCard")
-        centers = self.solve_heur(k)
-
-        # TODO: Implement me!
-
-        return centers
+        previous_solution = []
+        count = 0
+        m = max(self.distances.values())
+        prev_m = m
+        centers = self.solve_heur(k, m)
+        while len(centers) > 0:
+            #print(len(centers))
+            m = 0
+            selected = [self.reverse[self.x[i]] for i in range(len(centers)) if centers[i] > 0]
+            for c in selected:
+                for v in self.graph.nodes:
+                    if c != v:
+                        if m < self.distances[(c, v)] and self.distances[(c, v)] < prev_m:
+                            m = self.distances[(c, v)]
+            print(f"m,pn: {m, prev_m, m==prev_m}")
+            prev_m = m
+            previous_solution = centers
+            centers = self.solve_heur(k, m)
+            count += 1
+        print(f"prev: {previous_solution}")
+        print(f"count: {count}")
+        solution = [i for i in previous_solution if i > 0]
+        print(solution)
+        #print(min(len(self.connections[e]) for e in self.connections))
+        return solution
